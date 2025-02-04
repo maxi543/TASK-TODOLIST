b8c67a1227ff91ea5026ee44bf8277e33c8e22ce
maxi543 <140620549+maxi543@users.noreply.github.com>
2025-02-04 06:31:22 +0100

    Update server.js

--- user/server.js
+++ user/server.js
@@ -7,7 +7,18 @@ const session = require("express-session");
 const cookieParser = require("cookie-parser");
 
 const path = require("path");
-const routes = require("./routes");
+const routes = require("./routes");app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "dev123",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      maxAge: 6000 * 60 * 1000,
+    },
+  })
+);
 
 const app = express();
 const PORT = 5000;
@@ -35,15 +46,17 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(express.json()); // Parse JSON bodies
 app.use(cookieParser());
+app.set('trust proxy', 1); // Trust first proxy
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "dev123",
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: true, // Only send over HTTPS
       httpOnly: true,
       maxAge: 6000 * 60 * 1000,
+      sameSite: 'none' // Necessary when front end is on a different domain to backend
     },
   })
 );
